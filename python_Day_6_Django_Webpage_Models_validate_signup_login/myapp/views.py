fd614599f2fb01328bfc6b2c921cc849cc7112f9
pratham <jhahiren36@gmail.com>
2023-07-26 13:18:46 +0530

    day6

--- python_Day_6_Django_Webpage_Models_validate_signup_login/myapp/views.py
+++ python_Day_6_Django_Webpage_Models_validate_signup_login/myapp/views.py
@@ -10,17 +10,16 @@ def signup(request):
         p = request.POST['password']
         obj = Emp(uname=u,email=e,password=p)
         obj.save()
-        return redirect('/#')
+        return redirect("/#")
     return render(request,"signup.html")
 def login(request):
     if request.POST:
         e = request.POST['email']
         p = request.POST['password']
-        count = Emp.objects.filter(email=e,password=p).count()
+        count=Emp.objects.filter(email=e,password=p).count()
         if count>0:
-            return redirect('/home')
-    return render(request,"login.html")
+            return redirect("/home")
+    return render(request, "login.html")
+
 def home(request):
     return render(request,"home.html")
-
-
--- python_Day_6_Django_Webpage_Models_validate_signup_login/webapp/urls.py
+++ python_Day_6_Django_Webpage_Models_validate_signup_login/webapp/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup', views.signup),
-    path('',views.login),
-    path('home',views.home),
-
+    path('', include('myapp.urls')),
 ]
